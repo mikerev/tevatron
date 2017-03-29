fead7cf49e0c3446d300b81381f51a08a85b30c2
Mike Revelle <mike.revelle@tastytrade.com>
2017-03-29 10:26:48 -0500

    Update crazytard.py

--- crazytard.py
+++ crazytard.py
@@ -7,7 +7,7 @@ tardbase = "/home/tevatron/custom_modules/crazytard.txt"
 
 @module.rule('')
 def tard(bot, trigger):
-    if trigger.nick == 'CrazyMug' or 'CrazyTard':
+    if trigger.nick == 'CrazyMug':
         f = open(tardbase, 'r')
         line = random.choice(list(open(tardbase)))
         bot.say(line)
