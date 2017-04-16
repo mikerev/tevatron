7aa489c4dd602d13fd407b846e1ff1323836b428
Mike Revelle <mike.revelle@tastytrade.com>
2017-04-15 19:48:31 -0500

    Update personality.py

--- personality.py
+++ personality.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import praw, random, sys
+from sopel import module
 
 reddit = praw.Reddit(client_id='CLIENTID', client_secret='CLIENTSECRET', user_agent='USERAGENT')
 
@@ -7,10 +8,12 @@ subreddit = reddit.subreddit('chicago')
 random_post_number = random.randint(0,100)
 posts = subreddit.new(limit=100)
 
-for i,post in enumerate(posts):
-    if i==random_post_number:
-        submission = reddit.submission(id=post.id)
-        for top_level_comment in submission.comments:
-            comments = top_level_comment.body.splitlines()
-            print random.choice(comments)
-            break
+@module.rule('hello?')
+def hi(bot, trigger):
+    for i,post in enumerate(posts):
+        if i==random_post_number:
+            submission = reddit.submission(id=post.id)
+            for top_level_comment in submission.comments:
+                comments = top_level_comment.body.splitlines()
+                bot.say(random.choice(comments))
+                break
