edac6478f376b7d794a2c5ec63507f1fa8ca0f83
Mike Revelle <mrevelle@dough.com>
2017-04-22 17:48:16 -0500

    add random responses

--- personality.py
+++ personality.py
@@ -4,16 +4,16 @@ from sopel import module
 
 reddit = praw.Reddit(client_id='CLIENTID', client_secret='CLIENTSECRET', user_agent='USERAGENT')
 
-subreddit = reddit.subreddit('chicago')
-random_post_number = random.randint(0,100)
-posts = subreddit.new(limit=100)
-
-@module.rule('revtest')
+@module.rule('Tevatron')
 def hi(bot, trigger):
+    subreddit = reddit.subreddit('chicago')
+    random_post_number = random.randint(0,100)
+    posts = subreddit.new(limit=100)
     for i,post in enumerate(posts):
         if i==random_post_number:
             submission = reddit.submission(id=post.id)
             for top_level_comment in submission.comments:
                 comments = top_level_comment.body.splitlines()
-                bot.say(random.choice(comments))
+                bot.say(trigger.nick + ': ' + random.choice(comments))
                 break
+
