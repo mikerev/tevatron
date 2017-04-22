cac094904f55377f97ff1481fd564b144febe8b8
Mike Revelle <mrevelle@dough.com>
2017-04-22 18:41:11 -0500

    fixup

--- personality.py
+++ personality.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import praw, random, sys
 from sopel import module
+from time import sleep
 
 reddit = praw.Reddit(client_id='CLIENTID', client_secret='CLIENTSECRET', user_agent='USERAGENT')
 
@@ -17,3 +18,17 @@ def hi(bot, trigger):
                 comments = top_level_comment.body.splitlines()
                 bot.say(random.choice(comments))
                 break
+
+@module.rule('Tevatron')
+def hi(bot, trigger):
+    subreddit = reddit.subreddit('chicago')
+    random_post_number = random.randint(0,100)
+    posts = subreddit.new(limit=100)
+    for i,post in enumerate(posts):
+        if i==random_post_number:
+            submission = reddit.submission(id=post.id)
+            for top_level_comment in submission.comments:
+                comments = top_level_comment.body.splitlines()
+                bot.say(trigger.nick + ': ' + random.choice(comments))
+                break
+
