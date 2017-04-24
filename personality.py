1287663b1fa85668404c429be651b97d7d1e5b4c
Mike Revelle <mike.revelle@tastytrade.com>
2017-04-23 20:24:10 -0500

    Update personality.py

--- personality.py
+++ personality.py
@@ -3,24 +3,29 @@ import praw, random, sys
 from sopel import module
 from time import sleep
 
-reddit = praw.Reddit(client_id='CLIENTID', client_secret='CLIENTSECRET', user_agent='USERAGENT')
+clientID = 'CLIENTID'
+clientSEC = 'CLIENTSECRET'
+userAGNT = 'USERAGENT'
 
-@module.rule('')
-def hi(bot, trigger):
-    sleep(random.randint(900,1800))
-    subreddit = reddit.subreddit('chicago')
-    random_post_number = random.randint(0,100)
-    posts = subreddit.new(limit=100)
-    for i,post in enumerate(posts):
-        if i==random_post_number:
-            submission = reddit.submission(id=post.id)
-            for top_level_comment in submission.comments:
-                comments = top_level_comment.body.splitlines()
-                bot.say(random.choice(comments))
-                break
+# @module.rule('')
+# def hi(bot, trigger):
+#     #sleep(random.randint(2,10))
+#     if trigger.nick == 'Toba':
+#        reddit = praw.Reddit(client_id=clientID, client_secret=clientSEC, user_agent=userAGNT)
+#        subreddit = reddit.subreddit('chicago')
+#        random_post_number = random.randint(0,100)
+#        posts = subreddit.new(limit=1)
+#        for i,post in enumerate(posts):
+#            if i==random_post_number:
+#                submission = reddit.submission(id=post.id)
+#                for top_level_comment in submission.comments:
+#                    comments = top_level_comment.body.splitlines()
+#                    bot.say(random.choice(comments))
+#                    break
 
-@module.rule('Tevatron')
-def hi(bot, trigger):
+@module.rule('Tevatron:')
+def tevatron(bot, trigger):
+    reddit = praw.Reddit(client_id=clientID, client_secret=clientSEC, user_agent=userAGNT)
     subreddit = reddit.subreddit('chicago')
     random_post_number = random.randint(0,100)
     posts = subreddit.new(limit=100)
@@ -31,4 +36,5 @@ def hi(bot, trigger):
                 comments = top_level_comment.body.splitlines()
                 bot.say(trigger.nick + ': ' + random.choice(comments))
                 break
+
 
