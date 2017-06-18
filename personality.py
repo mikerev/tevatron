dfde2d60893f89e98b8adc7360968e847ee86af7
Mike Revelle <mike.revelle@tastytrade.com>
2017-06-17 19:24:48 -0500

    Update personality.py

--- personality.py
+++ personality.py
@@ -15,5 +15,6 @@ def tevatron(bot, trigger):
     posts = subreddit.new(limit=100)
     for i,post in enumerate(posts):
         if i==random_post_number:
-            submission = reddit.submission(id=post.id)
-            bot.say(trigger.nick + ': ' + submission.comments[1].body)
+        submission = reddit.submission(id=post.id)
+        print submission.comments[0].body
+
