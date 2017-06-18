31250a70f78e09fba4353d5149e952ce4c5e49fa
Mike Revelle <mike.revelle@tastytrade.com>
2017-06-17 19:04:11 -0500

    Update personality.py

--- personality.py
+++ personality.py
@@ -7,22 +7,6 @@ clientID = 'CLIENTID'
 clientSEC = 'CLIENTSECRET'
 userAGNT = 'USERAGENT'
 
-# @module.rule('')
-# def hi(bot, trigger):
-#     #sleep(random.randint(2,10))
-#     if trigger.nick == 'Toba':
-#        reddit = praw.Reddit(client_id=clientID, client_secret=clientSEC, user_agent=userAGNT)
-#        subreddit = reddit.subreddit('chicago')
-#        random_post_number = random.randint(0,100)
-#        posts = subreddit.new(limit=1)
-#        for i,post in enumerate(posts):
-#            if i==random_post_number:
-#                submission = reddit.submission(id=post.id)
-#                for top_level_comment in submission.comments:
-#                    comments = top_level_comment.body.splitlines()
-#                    bot.say(random.choice(comments))
-#                    break
-
 @module.rule('Tevatron:')
 def tevatron(bot, trigger):
     reddit = praw.Reddit(client_id=clientID, client_secret=clientSEC, user_agent=userAGNT)
@@ -31,10 +15,5 @@ def tevatron(bot, trigger):
     posts = subreddit.new(limit=100)
     for i,post in enumerate(posts):
         if i==random_post_number:
-            submission = reddit.submission(id=post.id)
-            for top_level_comment in submission.comments:
-                comments = top_level_comment.body.splitlines()
-                bot.say(trigger.nick + ': ' + random.choice(comments))
-                break
-
-
+        submission = reddit.submission(id=post.id)
+        bot.say(trigger.nick + ': ' + submission.comments[1].body
